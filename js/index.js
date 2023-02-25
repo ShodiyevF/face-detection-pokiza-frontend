1b0477853396be067bc7756c766b078318cc50a4
Abu _WEB <sodiqbek2002@gmail.com>
2023-02-25 17:45:13 +0500

    format code changed

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
-import { alert } from "../utils/alert.js"
-import {domain} from '../js/DOMAIN.js'
+import { alert } from '../utils/alert.js'
+import { domain } from '../js/DOMAIN.js'
 
 const form = document.querySelector('.form')
 const input_login = document.querySelector('#inputLogin')
@@ -10,29 +10,29 @@ if (form) {
     form.addEventListener('submit', async (e) => {
         e.preventDefault()
         input_login.style.borderColor = '#dee2e6'
-    
-        if(!input_login.value){
+        
+        if (!input_login.value) {
             input_login.style.borderColor = 'red'
-        } else if(!input_password.value){
+        } else if (!input_password.value) {
             input_password.style.borderColor = 'red'
         } else {
             const response = await fetch(domain + '/api/login', {
                 method: 'POST',
                 headers: {
-                    'Content-Type': 'application/json'
+                    'Content-Type': 'application/json',
                 },
                 body: JSON.stringify({
                     email: inputLogin.value.toString(),
                     password: inputPassword.value.toString(),
-                })
+                }),
             })
             const data = await response.json()
-            console.log(data);
-            if(data.status == 200){
+            console.log(data)
+            if (data.status == 200) {
                 localStorage.setItem('token', data.access_token)
                 window.location = '/'
             } else {
-                if(alert_wrapper.childElementCount <= 2){
+                if (alert_wrapper.childElementCount <= 2) {
                     alert_wrapper.appendChild(alert(data.message, 10, 'danger'))
                 }
             }
--- js/select.js
+++ js/select.js
@@ -4,47 +4,47 @@ const selectChild = document.querySelector('.select_child')
 const childList = document.querySelector('.dropdown_list')
 
 async function setItem() {
-  const res = await fetch(domain + '/api/branch')
-  const data = await res.json()
-
-  for (const i of data) {
-    const li = document.createElement('li')
-    li.classList.add('child')
-    li.dataset.id = i.id
-    li.textContent = i.branchName
-    childList.appendChild(li)
-  }
-
-  const childElement = document.querySelectorAll('.child')
-  for (const i of childElement) {
-    i.onclick = (e) => {
-      console.log(e.target.classList)
-      const classes = []
-      for (const i of e.target.classList) {
-        classes.push(i)
-      }
-      const finded = classes.find((el) => el == 'child_active')
-      if (finded) {
-        e.target.classList.remove('child_active')
-        e.target.dataset.activeBranch = false
-      } else {
-        e.target.dataset.activeBranch = true
-        e.target.classList.add('child_active')
-      }
+    const res = await fetch(domain + '/api/branch')
+    const data = await res.json()
+    
+    for (const i of data) {
+        const li = document.createElement('li')
+        li.classList.add('child')
+        li.dataset.id = i.id
+        li.textContent = i.branchName
+        childList.appendChild(li)
     }
-  }
+    
+    const childElement = document.querySelectorAll('.child')
+    for (const i of childElement) {
+        i.onclick = (e) => {
+            console.log(e.target.classList)
+            const classes = []
+            for (const i of e.target.classList) {
+                classes.push(i)
+            }
+            const finded = classes.find((el) => el == 'child_active')
+            if (finded) {
+                e.target.classList.remove('child_active')
+                e.target.dataset.activeBranch = false
+            } else {
+                e.target.dataset.activeBranch = true
+                e.target.classList.add('child_active')
+            }
+        }
+    }
 }
 setItem()
 
 mainSelect.onclick = (e) => {
-  if (e.target.dataset.role == 'father') {
-    const ca = selectChild.style.display
-    if (ca == 'none') {
-      selectChild.style.display = 'block'
-    } else {
-      selectChild.style.display = 'none'
+    if (e.target.dataset.role == 'father') {
+        const ca = selectChild.style.display
+        if (ca == 'none') {
+            selectChild.style.display = 'block'
+        } else {
+            selectChild.style.display = 'none'
+        }
     }
-  }
 }
 
 function checkItem() {}
